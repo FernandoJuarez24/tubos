0bd1ea9e6165e2151bdda2f39cd6066fc101c2c0
FernandoJuarez24 <57969849+FernandoJuarez24@users.noreply.github.com>
2022-08-25 18:35:43 -0500

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,15 @@
+let on_off = true;
+
+window.onscroll = () => {
+    //HOME
+    if (window.pageYOffset >= 350) {
+    	document.querySelector('.home').style.right = '15px';
+    }else{
+    	document.querySelector('.home').style.right = '-100%';
+    }
+
+    document.querySelector('.home').addEventListener('click', () => {
+    	document.body.scrollTop = '0';
+    	document.documentElement.scrollTop = '0';
+    });
+}
